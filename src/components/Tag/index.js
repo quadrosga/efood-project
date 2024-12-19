cbe2ba4b18b52d427334ab61d8b546ee5a673cc1
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2024-12-19 16:32:50 -0300

    criando tags

---
+++ src/components/Tag/index.js
@@ -0,0 +1,5 @@
+import { TagContainer } from "./styles";
+
+const Tag = ({ children }) => <TagContainer>{children}</TagContainer>;
+
+export default Tag;
---
+++ src/components/Tag/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { colors } from "../../styles";
+
+export const TagContainer = styled.div`
+  padding: 4px 6px;
+  background-color: ${colors.hotPink};
+  color: ${colors.lightPink};
+  font-weight: bold;
+  display: inline-block;
+  margin-left: 8px;
+  font-size: 14px;
+`;
