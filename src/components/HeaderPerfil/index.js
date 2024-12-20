0ddff2b1795964ccbdf0a20a5e751c50d3eba5d1
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2024-12-20 09:21:17 -0300

    criando banner do perfil do restaurante

--- src/components/HeaderPerfil/index.js
+++ src/components/HeaderPerfil/index.js
@@ -1,20 +1,34 @@
-import logo from "../../assets/images/logo.png";
-import { HeaderBar, Title, LinkCart } from "./styles";
-import headerBarImg from "../../assets/images/banner.png";
 import { Link } from "react-router-dom";
+import {
+  HeaderBar,
+  Title,
+  LinkCart,
+  Banner,
+  BannerP,
+  BannerTitle,
+} from "./styles";
+import logo from "../../assets/images/logo.png";
+import headerBarImg from "../../assets/images/banner.png";
+import headerBanner from "../../assets/images/imagem_fundo.png";
 
 const HeaderPerfil = () => (
-  <HeaderBar style={{ backgroundImage: `url(${headerBarImg})` }}>
-    <Title>
-      <p>Restaurantes</p>
-    </Title>
-    <div>
-      <Link to="/">
-        <img src={logo} alt="EFOOD" />
-      </Link>
-    </div>
-    <LinkCart href="#">0 - produtos no carrinho</LinkCart>
-  </HeaderBar>
+  <>
+    <HeaderBar style={{ backgroundImage: `url(${headerBarImg})` }}>
+      <Title>
+        <p>Restaurantes</p>
+      </Title>
+      <div>
+        <Link to="/">
+          <img src={logo} alt="EFOOD" />
+        </Link>
+      </div>
+      <LinkCart href="#">0 - produtos no carrinho</LinkCart>
+    </HeaderBar>
+    <Banner style={{ backgroundImage: `url(${headerBanner})` }}>
+      <BannerP>Italiana</BannerP>
+      <BannerTitle>Nome do restaurante</BannerTitle>
+    </Banner>
+  </>
 );
 
 export default HeaderPerfil;
--- src/components/HeaderPerfil/styles.js
+++ src/components/HeaderPerfil/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { colors } from "../../styles";
 
 export const HeaderBar = styled.div`
   background-repeat: no-repeat;
@@ -8,13 +9,38 @@ export const HeaderBar = styled.div`
   align-items: center;
   justify-content: space-between;
   padding: 82px 170px;
-`;
-
-export const Title = styled.div`
   font-size: 18px;
   font-weight: 900;
 `;
 
+export const Title = styled.div`
+  color: ${colors.hotPink};
+`;
+
 export const LinkCart = styled.a`
   display: flex;
+  text-decoration: none;
+  color: ${colors.hotPink};
+`;
+
+export const Banner = styled.div`
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 280px;
+  color: ${colors.white};
+  font-size: 32px;
+  position: relative;
+`;
+
+export const BannerP = styled.p`
+  padding-top: 24px;
+  padding-left: 170px;
+  font-weight: 100;
+`;
+
+export const BannerTitle = styled.div`
+  position: absolute;
+  bottom: 32px;
+  left: 170px;
+  font-weight: 900;
 `;
